ccc7358bcd0221ee1a83f8512e8405ee9e95da39
techabyte <techabyte@users.noreply.github.com>
2017-03-28 16:14:03 +1100

    Player class to store names

---
+++ src/model/Player.java
@@ -0,0 +1,14 @@
+package model;
+
+/** Main class to store player name and all relevant details on each player.
+ * Extend player hand here?
+ */
+
+public class Player {
+    private String name;
+    private boolean role;
+
+    public void Player (String PlayerName) {
+        name = PlayerName;
+    }
+}
