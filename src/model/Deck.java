7a430fd8874dd35bdbaf4128525ded6b0ef4cd74
techabyte <techabyte@users.noreply.github.com>
2017-04-02 20:04:55 +1000

    Deck Singleton

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -6,25 +6,32 @@ import java.util.Stack;
 import model.cards.*;
 
 public class Deck {
+	private static Deck ourInstance = new Deck();
+
+	public static Deck getInstance() {
+		return ourInstance;
+	}
 
 
 	private Stack<Card> deck = new Stack<Card>();
-	
+
 	public Deck(){
-		
+
 		//add the following cards to the deck
 		//where the passed int is the number of cards
 		addLPathCards(4);
 		addXPathCards(4);
 		addStraightPathCards(4);
 		addBlockPathCards(4);
+		addTPathCards(4);
+
 
 	}
 	public void shuffle(){
 		//shuffle all cards in deck
 		Collections.shuffle(deck);
 	}
-	
+
 	public Card draw(){
 		//drawn next card from deck
 		//return null if no cards in deck
@@ -39,11 +46,11 @@ public class Deck {
 		System.out.println("cards2 stack printout: ");
 		Iterator<Card> deckIteration = deck.iterator();
 		while (deckIteration.hasNext()){
-		    System.out.println(deckIteration.next());
+			System.out.println(deckIteration.next());
 		}
 	}
-	
-	
+
+
 	private void addLPathCards(int totalCards){
 		System.out.println("addingLPC...");
 		for(int i = 0; i<totalCards; i++){
@@ -68,6 +75,14 @@ public class Deck {
 		for(int i = 0; i<totalCards; i++){
 			StraightPathCard straightCard = new StraightPathCard(0);
 			deck.push(straightCard);
+		}
+	}
+	private void addTPathCards(int totalCards){
+		System.out.println("addingTPC...");
+		for(int i = 0; i<totalCards; i++){
+			System.out.println("TPC added");
+			TPathCard tPathCard = new TPathCard(0);
+			deck.push(tPathCard);
 		}
 	}
 
