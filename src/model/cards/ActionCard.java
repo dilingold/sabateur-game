9d7703963e11fa3de4164a42073c556aa2be73bb
techabyte <techabyte@users.noreply.github.com>
2017-04-08 16:48:25 +1000

    Quick Fix Revert Action Card

--- src/model/cards/ActionCard.java
+++ src/model/cards/ActionCard.java
@@ -1,18 +1,20 @@
 package model.cards;
 
 public abstract class ActionCard extends Card {
-	
+
 	public String type = "action";
 	protected String effect;
-	
-	public ActionCard(){
-		this.type = "action";
-	}
-	
+
 	public String getEffect() {
-		
+
 		return effect;
-		
+
 	}
-	
+
+	public String getType() {
+
+		return type;
+
+	}
+
 }
