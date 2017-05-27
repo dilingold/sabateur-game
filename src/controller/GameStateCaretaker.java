9720ae6c1cf112935bf644a1252dccdeba92113a
Tristan Boyd <s3547443@student.rmit.edu.au>
2017-05-27 21:53:39 +1000

    refactored Mememto model to accurately reflect pattern

--- src/controller/GameStateCaretaker.java
+++ src/controller/GameStateCaretaker.java
@@ -6,108 +6,46 @@ import java.util.Map;
 import java.util.Stack;
 
 import model.Board;
-import model.Deck;
 import model.Player;
 import model.cards.Card;
-import model.cards.PathCard;
-import view.AddPlayerView;
-import view.GameView;
-import view.RefreshBoard;
 
 public class GameStateCaretaker {
 
     static Map<Integer, Stack<Card>> deckStates = new HashMap<Integer, Stack<Card>>();
     static Map<Integer, Board> boardStates = new HashMap<Integer, Board>();
     static Map<Integer, ArrayList<Player>> playerStates = new HashMap<Integer, ArrayList<Player>>();
-    static int turn = 0;
-    static int currentPlayerIndex = 0;
     static Map<Integer, ArrayList<ArrayList<Card>>> handStates = new HashMap<Integer, ArrayList<ArrayList<Card>>>();
-    private static int numberOfRegressions = 0;
-    // arraylist of ints of stateIDs used
-    // when load state, delete ones after one loaded
-    // when load state, look for..
-    // maybe map<int, int> of stateID, regression?
-    static Stack<Integer> priorStates = new Stack<Integer>();
+
+
 
     public GameStateCaretaker() {
     }
-
-    public static void saveState() {
-        currentPlayerIndex = GameEngine.getCurrentPlayerIndex();
-        int stateID = generateStateID();
-        priorStates.push(stateID);
-        boardStates.put(stateID, GameStateMemento.getBoardState());
-        playerStates.put(stateID, GameStateMemento.getPlayerState());
-        Stack<Card> deckState = GameStateMemento.saveDeck();
-        handStates.put(stateID, GameStateMemento.getPlayerHands());
+    public static void saveDeckState(int stateID, Stack<Card> deckState){
         deckStates.put(stateID, deckState);
-        // For debugging: printout of saved board state
-        System.out.println("State " + stateID + " saved. BoardState: ");
-        for (int r = 0; r < boardStates.get(stateID).getRows(); r++) {
-            for (int c = 0; c < boardStates.get(stateID).getCols(); c++) {
-                System.out.println("Cardname: " + boardStates.get(stateID).getCard(r, c).getName());                
-                if (boardStates.get(stateID).getCard(r, c).getType() == "path") {
-                    if (((PathCard) boardStates.get(stateID).getCard(r, c)).getIsToxic() == true) {
-                        System.out.println("This card is toxic");
-                    }
-
-                }
-            }
-        }
-
     }
-
-    public void loadState(int turnsReverted) {
-
-        System.out.println("Rewinding turn: going to turn " + (GameEngine.getTurn() - turnsReverted));
-        int oldTurn = (GameEngine.getTurn() - turnsReverted);
-        System.out.println("turnID: " + oldTurn);
-        GameEngine.setTurn(oldTurn);
-        int oldStateID = generateStateID(turnsReverted);
-        Deck.setDeck(deckStates.get(oldStateID));
-        PlayerController.setPlayers(playerStates.get(oldStateID));
-        PlayerController.setHand(handStates.get(oldStateID));
-        Board.setBoard(boardStates.get(oldStateID));
-
-        PlayGameListener.changeScene(AddPlayerView.getStage());
-
-        RefreshBoard refreshBoard = new RefreshBoard();
-        refreshBoard.refreshView();
-        numberOfRegressions++;
-        System.out.println("State " + oldStateID + " loaded. BoardState: ");
-        for (int r = 0; r < boardStates.get(oldStateID).getRows(); r++) {
-            for (int c = 0; c < boardStates.get(oldStateID).getCols(); c++) {
-                System.out.println("Cardname: " + boardStates.get(oldStateID).getCard(r, c).getName());
-                if (boardStates.get(oldStateID).getCard(r, c).getType() == "path") {
-                    if (((PathCard) boardStates.get(oldStateID).getCard(r, c)).getIsToxic() == true) {
-                        System.out.println("This card is toxic");
-                    }
-
-                }
-            }
-        }
+    public static Stack<Card> getDeckState(int oldStateID){
+        return deckStates.get(oldStateID);
     }
-
-    private static int generateStateID() {
-        System.out.println(
-                "turn: " + GameEngine.getTurn() + " Player: " + currentPlayerIndex + " numReg: " + numberOfRegressions);
-        String stateID = Integer.toString(GameEngine.getTurn()) + Integer.toString(currentPlayerIndex)
-                + Integer.toString(numberOfRegressions);
-        System.out.println("State generated: " + stateID);
-        return Integer.parseInt(stateID);
+    public static void savePlayerState(int stateID, ArrayList<Player> playerState){
+        playerStates.put(stateID, playerState);
     }
-
-    private static int generateStateID(int turnsReverted) {
-        // String stateID =
-        // Integer.toString(turn-turnsReverted)+Integer.toString(currentPlayerIndex)+Integer.toString(numberOfRegressions);
-        int numberPlayers = GameEngine.getPlayerSize();
-        for (int i = 0; i < ((turnsReverted * numberPlayers) - 1); i++) {
-            int statediscarded = priorStates.pop();
-            System.out.println("State discarded: " + statediscarded);
-        }
-        String stateID = Integer.toString(priorStates.pop());
-        System.out.println("State to load generated: " + stateID);
-        return Integer.parseInt(stateID);
+    public static ArrayList<Player> getPlayerState(int oldStateID){
+        return playerStates.get(oldStateID);
     }
+    public static void saveBoardState(int stateID, Board boardState){
+        boardStates.put(stateID, boardState);
+    }
+    public static Board getBoardState(int oldStateID){
+        return boardStates.get(oldStateID);
+    }
+    public static void saveHandListState(int stateID, ArrayList<ArrayList<Card>> handState){
+        handStates.put(stateID, handState);
+    }
+    public static ArrayList<ArrayList<Card>> getHandListState(int oldStateID){
+        return handStates.get(oldStateID);
+    }
+    
+    
+   
 
 }
---
+++ src/controller/GameStateOriginator.java
@@ -0,0 +1,89 @@
+package controller;
+
+import java.util.Stack;
+
+import model.cards.Card;
+
+public class GameStateOriginator {
+    static int turn = 0;
+    static int currentPlayerIndex = 0;
+    private static int numberOfRegressions = 0;
+    static Stack<Integer> priorStates = new Stack<Integer>();
+    
+    
+ 
+    
+    public static void saveState() {
+        currentPlayerIndex = GameEngine.getCurrentPlayerIndex();
+        int stateID = generateStateID();
+        priorStates.push(stateID);
+        GameStateCaretaker.saveBoardState(stateID, GameStateMemento.getBoardState());
+        GameStateCaretaker.savePlayerState(stateID, GameStateMemento.getPlayerState());
+        GameStateCaretaker.saveHandListState(stateID, GameStateMemento.getPlayerHands());
+        GameStateCaretaker.saveDeckState(stateID, GameStateMemento.saveDeck());
+        // For debugging: printout of saved board state
+       /* System.out.println("State " + stateID + " saved. BoardState: ");
+        for (int r = 0; r < boardStates.get(stateID).getRows(); r++) {
+            for (int c = 0; c < boardStates.get(stateID).getCols(); c++) {
+                System.out.println("Cardname: " + boardStates.get(stateID).getCard(r, c).getName());                
+                if (boardStates.get(stateID).getCard(r, c).getType() == "path") {
+                    if (((PathCard) boardStates.get(stateID).getCard(r, c)).getIsToxic() == true) {
+                        System.out.println("This card is toxic");
+                    }
+
+                }
+            }
+        }*/
+
+    }
+
+    public void loadState(int turnsReverted) {
+
+       
+        int oldTurn = (GameEngine.getTurn() - turnsReverted);
+        System.out.println("Rewinding turn: going to turn " + oldTurn);
+        GameStateMemento.setTurn(oldTurn);
+        int oldStateID = generateStateID(turnsReverted);
+        GameStateMemento.setDeck(GameStateCaretaker.getDeckState(oldStateID));
+        GameStateMemento.setPlayers(GameStateCaretaker.getPlayerState(oldStateID));
+        GameStateMemento.setHand(GameStateCaretaker.getHandListState(oldStateID));
+        GameStateMemento.setBoard(GameStateCaretaker.getBoardState(oldStateID));
+        GameStateMemento.refreshView();
+        numberOfRegressions++;
+     // For debugging: printout of loaded board state
+        /*System.out.println("State " + oldStateID + " loaded. BoardState: ");
+        for (int r = 0; r < boardStates.get(oldStateID).getRows(); r++) {
+            for (int c = 0; c < boardStates.get(oldStateID).getCols(); c++) {
+                System.out.println("Cardname: " + boardStates.get(oldStateID).getCard(r, c).getName());
+                if (boardStates.get(oldStateID).getCard(r, c).getType() == "path") {
+                    if (((PathCard) boardStates.get(oldStateID).getCard(r, c)).getIsToxic() == true) {
+                        System.out.println("This card is toxic");
+                    }
+
+                }
+            }
+        }*/
+    }
+
+    private static int generateStateID() {
+        System.out.println(
+                "turn: " + GameEngine.getTurn() + " Player: " + currentPlayerIndex + " numReg: " + numberOfRegressions);
+        String stateID = Integer.toString(GameEngine.getTurn()) + Integer.toString(currentPlayerIndex)
+                + Integer.toString(numberOfRegressions);
+        System.out.println("State generated: " + stateID);
+        return Integer.parseInt(stateID);
+    }
+
+    private static int generateStateID(int turnsReverted) {
+        // String stateID =
+        // Integer.toString(turn-turnsReverted)+Integer.toString(currentPlayerIndex)+Integer.toString(numberOfRegressions);
+        int numberPlayers = GameEngine.getPlayerSize();
+        for (int i = 0; i < ((turnsReverted * numberPlayers) - 1); i++) {
+            int statediscarded = priorStates.pop();
+            System.out.println("State discarded: " + statediscarded);
+        }
+        String stateID = Integer.toString(priorStates.pop());
+        System.out.println("State to load generated: " + stateID);
+        return Integer.parseInt(stateID);
+    }
+}
